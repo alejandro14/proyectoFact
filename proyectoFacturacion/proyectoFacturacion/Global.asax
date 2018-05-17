0a3152cbe568a1fedc04839f6f4dfaeb1f4b3998
Alejandro Arce <alearce48@hotmail.com>
2018-05-16 20:02:40 -0600

    desarrollo

---
+++ proyectoFacturacion/proyectoFacturacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="proyectoFacturacion.MvcApplication" Language="C#" %>
---
+++ proyectoFacturacion/proyectoFacturacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(proyectoFacturacion.Startup))]
+namespace proyectoFacturacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
